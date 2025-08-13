f3debfc73fe02a17ca8fdfb7b5a3e8f029cc5d46
Eduardo Santana <ezambomsantana@gmail.com>
2025-08-13 11:32:43 -0300

    first tests

---
+++ src/test/java/br/insper/cotacao/stocks/service/StockServiceTest.java
@@ -0,0 +1,63 @@
+package br.insper.cotacao.stocks.service;
+
+import br.insper.cotacao.stocks.dto.StockDTO;
+import br.insper.cotacao.stocks.model.Stock;
+import br.insper.cotacao.stocks.repository.StockRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class StockServiceTest {
+
+    @InjectMocks
+    private StockService stockService;
+
+    @Mock
+    private StockRepository stockRepository;
+
+    @Test
+    public void test_listAllShouldReturnListEmpty() {
+
+        // mock
+        Mockito.when(stockRepository.findAll()).thenReturn(new ArrayList<>());
+
+        // chamada funcao
+        List<StockDTO> resp = stockService.listAll();
+
+        // verificacoes
+        Assertions.assertNotNull(resp);
+        Assertions.assertTrue(resp.isEmpty());
+
+    }
+
+    @Test
+    public void test_listAllShouldReturnListWithTwoElements() {
+
+        Stock stock = new Stock();
+        stock.setName("Petrobras");
+        stock.setTicker("PETR");
+
+        // mock
+        Mockito.when(stockRepository.findAll()).thenReturn(List.of(stock));
+
+        // chamada funcao
+        List<StockDTO> resp = stockService.listAll();
+
+        // verificacoes
+        Assertions.assertNotNull(resp);
+        Assertions.assertFalse(resp.isEmpty());
+        Assertions.assertEquals(1, resp.size());
+        Assertions.assertEquals("PETR", resp.get(0).ticker());
+    }
+
+
+
+}
